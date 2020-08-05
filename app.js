15fcf420a13b638d170df1c975a9c0b4c232456e
Jenny Chou <jmychou@gmail.com>
2020-08-04 20:36:56 -0700

    Node Callbacks and Error Handling

--- app.js
+++ app.js
@@ -5,17 +5,20 @@
 //     area: (x, y) => x * y
 // }
 
-const rect = require('./rectangle');            // "require" keyword is from Node. What's inside the () tells code to go into the rectangle.js file within the same folder and import its exports to set as this rect object's properties/methods  
+const rect = require('./rectangle');            // "require" keyword is from Node. What's inside the () tells code to go into the rectangle.js file within the same folder and import its exports; now that the rectangle.js module is a function, const rect will also be a function (whereas it was previously an object)   
 
 function solveRect(l, w) {
     console.log (`Solving for rectangle with dimentions: ${l}, ${w}`);              // Use template literal with backticks; this is a more concise way of a string
 
-    if (l <= 0 || w <= 0) {
-        console.log(`Rectangle dimensions must be greater than zero. Received: ${l}, ${w}`)
-    } else {
-        console.log(`Area of rectangle: ${rect.area(l,w)} `);
-        console.log(`Perimeter of rectangle: ${rect.perimeter(l,w)}`);
-    }
+    rect(l, w, (err, rectangle) => {
+        if (err) {
+            console.log('ERROR:', err.message);
+        } else {
+            console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()} `);
+            console.log(`Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`);
+        }
+    });
+    console.log(`This statemnet is logged after the call to rect()`);
 }
 
 solveRect(2,4);
--- rectangle.js
+++ rectangle.js
@@ -1,4 +1,21 @@
-// This is a module with two exports. Created module by moving parimeter and area object properties from app.js file
+// This is a Node module
+// "exports" keyword is shorthand for "module.exports" and can be used when defining methods on the export like we did previously
+// However, here we're assigning a function to the ENTIRE export, rather than methods to be exported, so must use modeule.export
 
-exports.perimeter = (x, y) => 2 * (x + y);              // Create module. Can use "module.exports" keyword from Node, or its shorthand version, "exports" keyword 
-exports.area = (x,y) => (x * y);                        // "exports" keyword is followed by method name "perimeter" and "area"
+
+module.exports = (x, y, callback) => {                 // module.exports function has 3 params (x, y, callback)
+    // First, check for errors; if there are any, execute callback function (3rd param in module.exports) and give it the Error object as its first param
+    if (x <= 0 || y <= 0) {                               
+        callback(new Error(`Rectangle dimensions must be greater than zero. Received: ${x}, ${y}`));                 // It's best practice to set callback fxn's first argument as the Error object. Error object is a global object in JS that contains info about an error   
+    // If no errors, simulate a delay then call same callback function but with null as first argument and 2nd argument is an object computing perimeter and area 
+    } else {
+        setTimeout(() =>
+            callback(null, {
+                perimeter: () => 2 * (x + y),             
+                area: () => (x * y),
+            }),
+            2000
+        )
+    }
+};
+
